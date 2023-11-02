bcfa9c67dffc1c507095288cc48408e42c3d6bbf
Teja <tejalal.choudhary@gmail.com>
2023-11-02 16:12:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.sidebar.image("https://www.google.com/imgres?imgurl=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FDs29CXjXgAE_XEy.jpg&tbnid=rP-5z5GC7hZrpM&vet=12ahUKEwjA58y2j6WCAxXV7jgGHbptBDEQMygGegQIARBa..i&imgrefurl=https%3A%2F%2Ftwitter.com%2Ftejalal%2Fstatus%2F1066716564236447748&docid=8EaUFvDhoKs1GM&w=800&h=1200&q=tejalal%20choudhary&ved=2ahUKEwjA58y2j6WCAxXV7jgGHbptBDEQMygGegQIARBa", use_column_width=True)
-st.write("Hellow Streamlit")
+a = st.sidebar.radio('Choose', ["Experience", "Education", "Research"])
+
+st.write("TEJALAL CHOUDHARY")

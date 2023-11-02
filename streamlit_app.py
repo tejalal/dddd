c56f94126d008264f3ed0517d4c99e4f5c14044e
Teja <tejalal.choudhary@gmail.com>
2023-11-02 16:18:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ st.write("TEJALAL CHOUDHARY")
 
 a = st.sidebar.radio('Choose', ["Experience", "Education", "Research"])
 
-tab1, tab2 = st.tabs(["Experience", "Research", "Publications", "Education"])
+tab1, tab2, tab3, tab4 = st.tabs(["Experience", "Research", "Publications", "Education"])
 tab1.write("Experience")
 tab2.write("Research")
-tab2.write("Publications")
-tab2.write("Education")
+tab3.write("Publications")
+tab4.write("Education")
 
 
